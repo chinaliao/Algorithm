6e8f0fda4b112d3f46c8e6e5f6cf2a6dc3d1ad9c
chinaliao <15779042409@139.com>
2025-08-08 16:41:57 +0800

    DynamicProgramming(DP)练习

--- Suanfa/src/DP/DifferentPaths.java
+++ Suanfa/src/DP/DifferentPaths.java
@@ -1,9 +1,35 @@
 package DP;
 
+import java.util.Scanner;
+
 /**
  * ClassName:DifferentPaths
  * Description:机器人从左上角 (0,0) 走到右下角 (m-1, n-1)，每次只能向下或向右走，问有多少种走法。
  */
 public class DifferentPaths {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int m = scanner.nextInt(); // 行数
+        int n = scanner.nextInt(); // 列数
+        
+        // dp[i][j] 表示从(0,0)到(i,j)的路径数
+        int[][] dp = new int[m][n];
+        
+        // 初始化第一行和第一列  确保边界条件
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        for (int j = 0; j < n; j++) {
+            dp[0][j] = 1;
+        }
+        
+        // 填充dp表
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        
+        System.out.println(dp[m-1][n-1]);
+    }
 }
--- Suanfa/src/DP/Lis.java
+++ Suanfa/src/DP/Lis.java
@@ -1,9 +1,35 @@
 package DP;
 
+import java.util.Arrays;
+
 /**
  * ClassName:Lis
  * Description:给定数组 [10, 9, 2, 5, 3, 7, 101, 18]，求最长严格递增子序列的长度。
  */
 public class Lis {
+        public int lengthOfLIS(int[] nums){
+            if (nums == null||nums.length==0) {
+                return 0;
+            }
+            int[] dp = new int[nums.length];
+            int maxLen = 1;
+            Arrays.fill(dp,1);
+        for (int i = 1;i<nums.length;i++){
+            for (int j = 0; j < i; j++) {
+                if (nums[i]>nums[j]){
+                    dp[i]= Math.max(dp[i],dp[j]+1 );
+                }
+            }
+            maxLen = Math.max(maxLen,dp[i]);
+        }
+        return maxLen;
+        }
 
+    public static void main(String[] args) {
+        int[] nums = new int[]{10,9, 2, 5, 3, 7, 101, 18};
+        Lis lis = new Lis();
+        int length = lis.lengthOfLIS(nums);
+        System.out.println("maxlength = " + length);
+
+    }
 }
